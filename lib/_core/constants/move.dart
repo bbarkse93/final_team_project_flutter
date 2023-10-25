52eea2a2a27c132469ee73fca0c29e211dfd1d1d
bbarkse93 <bbarkse93@gmail.com>
2023-10-25 18:39:03 +0900

    상품 등록 그림 완료

--- lib/_core/constants/move.dart
+++ lib/_core/constants/move.dart
@@ -3,6 +3,7 @@ import 'package:team_project/ui/pages/auth/join_page/join_page.dart';
 import 'package:team_project/ui/pages/auth/login_page/login_page.dart';
 import 'package:team_project/ui/pages/main_page.dart';
 import 'package:team_project/ui/pages/post/list_page/post_list_page.dart';
+import 'package:team_project/ui/pages/product/product_write_page.dart';
 import 'package:team_project/ui/pages/splash_page/splash_page.dart';
 
 class Move {
@@ -11,6 +12,7 @@ class Move {
   static String splashPage = "/splash";
   static String postListPage = "/post/list";
   static String mainPage = "/main";
+  static String productWritePage = "/product/write";
 }
 
 Map<String, Widget Function(BuildContext)> getRouters() {
@@ -20,5 +22,6 @@ Map<String, Widget Function(BuildContext)> getRouters() {
     Move.splashPage: (context) => const SplashPage(),
     Move.postListPage: (context) => PostListPage(),
     Move.mainPage: (context) => MainPage(),
+    Move.productWritePage: (context) => ProductWritePage(),
   };
 }
--- lib/_core/constants/theme.dart
+++ lib/_core/constants/theme.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 
+MaterialColor primaryWhite = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
+
 ThemeData theme() {
   return ThemeData(
+    primarySwatch: primaryWhite,
     appBarTheme: appBarTheme(),
   );
 }
 
 AppBarTheme appBarTheme() {
   return const AppBarTheme(
-    titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+    titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
     centerTitle: true,
-    backgroundColor: Color.fromRGBO(255, 126, 00, 1),
+    backgroundColor: Color.fromRGBO(255, 255, 255, 1),
     elevation: 1,
   );
 }
