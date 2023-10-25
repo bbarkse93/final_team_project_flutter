db7db7f506893485d89e5e27bf6a0ee3491c05a4
wanni30 <hubhy@nate.com>
2023-10-25 12:48:35 +0900

    ㅕupdate joinFolder

--- lib/ui/pages/auth/join_page/widgets/join_body.dart
+++ lib/ui/pages/auth/join_page/widgets/join_body.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:team_project/_core/constants/move.dart';
-import 'package:team_project/_core/constants/size.dart';
-import 'package:team_project/ui/pages/auth/join_page/widgets/join_form.dart';
+import 'package:team_project/ui/pages/auth/join_page/widgets/join_form_field.dart';
+import 'package:team_project/ui/pages/auth/join_page/widgets/login_move_button.dart';
 import 'package:team_project/ui/widgets/custom_logo.dart';
-import 'package:team_project/ui/widgets/custom_text_button.dart';
 
 class JoinBody extends StatelessWidget {
   const JoinBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView(
-        children: [
-          const SizedBox(height: xlargeGap),
-          const CustomLogo(title: "Carrot"),
-          const SizedBox(height: largeGap),
-          JoinForm(),
-          CustomTextButton(
-            "로그인 페이지로 이동",
-            () {
-              Navigator.pushNamed(context, Move.loginPage);
-            },
-          ),
-        ],
-      ),
+    return ListView(
+      children: [
+        CustomLogo(title: "Carrot"),
+        JoinFormField(),
+        LoginMoveButton(),
+      ],
     );
   }
 }
--- lib/ui/widgets/custom_logo.dart
+++ lib/ui/widgets/custom_logo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:team_project/_core/constants/size.dart';
 
 class CustomLogo extends StatelessWidget {
   final String title;
@@ -7,18 +8,21 @@ class CustomLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(
-          "assets/carrot_image.png",
-          height: 100,
-          width: 100,
-        ),
-        Text(
-          title,
-          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(top: xlargeGap, bottom: largeGap),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/carrot_image.png",
+            height: 100,
+            width: 100,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
     );
   }
 }
