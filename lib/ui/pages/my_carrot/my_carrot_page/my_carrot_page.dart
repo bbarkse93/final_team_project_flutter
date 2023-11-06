9d022e6b69bc4c75f8001e5929865c48c5182ada
bong <darksen7693@gmail.com>
2023-11-06 11:18:12 +0900

    페이지 수정,앱바 수정완료

--- lib/ui/pages/my_carrot/my_carrot_page/my_carrot_page.dart
+++ lib/ui/pages/my_carrot/my_carrot_page/my_carrot_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:team_project/_core/constants/size.dart';
-import 'package:team_project/ui/pages/auth/my_carrot_page/my_carrot_page_widgets/my_carrot_activity.dart';
-import 'package:team_project/ui/pages/auth/my_carrot_page/my_carrot_page_widgets/my_carrot_app_bar.dart';
-import 'package:team_project/ui/pages/auth/my_carrot_page/my_carrot_page_widgets/my_carrot_info.dart';
-import 'package:team_project/ui/pages/auth/my_carrot_page/my_carrot_page_widgets/my_carrot_items.dart';
+import 'package:team_project/ui/pages/my_carrot/my_carrot_page/my_carrot_page_widgets/my_carrot_activity.dart';
+import 'package:team_project/ui/pages/my_carrot/my_carrot_page/my_carrot_page_widgets/my_carrot_app_bar.dart';
+import 'package:team_project/ui/pages/my_carrot/my_carrot_page/my_carrot_page_widgets/my_carrot_info.dart';
+import 'package:team_project/ui/pages/my_carrot/my_carrot_page/my_carrot_page_widgets/my_carrot_items.dart';
 
 class MyCarrotPage extends StatelessWidget {
   const MyCarrotPage({super.key});
--- lib/ui/widgets/forms/custom_search_text_form_field.dart
+++ lib/ui/widgets/forms/custom_search_text_form_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'package:team_project/_core/constants/size.dart';
-import 'package:team_project/ui/pages/auth/location_select_page/location_select_view_model.dart';
+import 'package:team_project/ui/pages/my_location/location_select_page/location_select_view_model.dart';
 
 class CustomSearchTextFormField extends ConsumerWidget
     implements PreferredSizeWidget {
