64afd9cce2d4980db30080a3317da56581af7c37
wanni30 <hubhy@nate.com>
2023-11-10 20:52:14 +0900

    what the this problem

--- lib/_core/constants/http.dart
+++ lib/_core/constants/http.dart
@@ -4,11 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 // http 통신
 final dio = Dio(
   BaseOptions(
-<<<<<<< HEAD
     baseUrl: "http://192.168.0.24:8080", // 내 IP 입력
-=======
-    baseUrl: "http://192.168.0.9:8080", // 내 IP 입력
->>>>>>> d309c8183053f5871a075dc621e76ee45b6f4a14
     contentType: "application/json; charset=utf-8",
   ),
 );
