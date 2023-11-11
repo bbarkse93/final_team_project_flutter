6d064b6190c28bf0c30d796e5fd150b0991014fc
wanni30 <hubhy@nate.com>
2023-11-11 10:46:56 +0900

    rebuild the logout

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
-      initialRoute: Move.mainPage,
+      initialRoute: Move.splashPage,
       routes: getRouters(),
       theme: theme(),
     );
